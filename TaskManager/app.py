424a63ff93dc1daa92a20ff2375b416370ac0aef
Your Name <you@example.com>
2025-11-07 00:52:12 +0530

    Connecting Flask and Postgresql

--- TaskManager/app.py
+++ TaskManager/app.py
@@ -1,6 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, session
+import psycopg2
+from psycopg2.extras import RealDictCursor
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
+bcrypt = Bcrypt(app)
+
+# Change this later to environment variable
+app.secret_key = "mysecretkey"
+
+def get_db_connection():
+    return psycopg2.connect(
+        host="localhost",
+        database="taskmanagerdb",
+        user="postgres",    # Change if your username is different
+        password="Your_Postgres_Password",  # <-- UPDATE THIS
+        cursor_factory=RealDictCursor
+    )
 
 @app.route("/")
 def home():
---
+++ TaskManager/taskmanagerdb.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE taskmanagerdb;
+CREATE TABLE users(
+	id SERIAL PRIMARY KEY,
+	username VARCHAR(100) UNIQUE NOT NULL,
+	password_hash TEXT NOT NULL
+);
+SELECT * from users;
